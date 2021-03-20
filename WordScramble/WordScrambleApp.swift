43bb2803d901285eb920bb1c1b80548cf29c6533
ntrillo <nospam@avaniu.com>
2021-03-20 11:24:11 -1000

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Nestor Trillo on 3/20/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
